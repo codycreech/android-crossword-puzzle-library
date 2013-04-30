049c2cd0afece3454395d882d7a3badd53ed83cc
Cody <codycreech@me.com>
2013-04-29 21:38:20 -0400

    added the Board class

---
+++ CrosswordPuzzleActivity/src/com/project/crosswordpuzzleactivity/Board.java
@@ -0,0 +1,16 @@
+package com.project.crosswordpuzzleactivity;
+
+public class Board {
+	
+	public static final int size = 15;
+	
+	String[][] board;
+	
+	public Board()
+	{
+		board = new String[size][size];
+	}
+	
+	
+
+}

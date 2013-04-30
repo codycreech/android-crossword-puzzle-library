b5e65bd43881c2bf71eeab1589332216fac89ed0
codycreech <codycreech@me.com>
2013-04-30 09:10:48 -0300

    Update Board.java

--- CrosswordPuzzleActivity/src/com/project/crosswordpuzzleactivity/Board.java
+++ CrosswordPuzzleActivity/src/com/project/crosswordpuzzleactivity/Board.java
@@ -2,6 +2,12 @@ package com.project.crosswordpuzzleactivity;
 
 public class Board {
 	
+	/**
+	 * @Board creates the board with a 15x15 grid.
+	 * 
+	 * @author codycreech
+	 */
+	
 	public static final int size = 15;
 	
 	String[][] board;
